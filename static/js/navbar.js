8794ca4ffa1e03f4766a66c4ca7cc405ec3aa374
Om Mishra <admin@projectrexa.ml>
2024-02-05 23:21:03 +0530

    UI Updates

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,8 +4,14 @@ const notifications = document.getElementById('notifications');
 const userProfileDropdown = document.getElementById('user-profile-dropdown');
 const notificationsDropdown = document.getElementById('notification-dropdown');
 
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const mobileNavClose = document.getElementById('mobile-nav-close');
+const mobileNavLinks = document.getElementById('mobile-nav-links');
+
 userProfileDropdown.style.display = 'none';
 notificationsDropdown.style.display = 'none';
+mobileNavClose.style.display = 'none';
+mobileNavLinks.style.display = 'none';
 
 
 function toggleDropdown(dropdown) {
@@ -38,4 +44,25 @@ if (notifications) {
     notificationsDropdown.addEventListener('mouseleave', () => {
         notificationsDropdown.style.display = 'none';
     });
+}
+
+
+if (hamburgerIcon) {
+    hamburgerIcon.addEventListener('click', () => {
+        if (mobileNavLinks.style.display === 'none') {
+            mobileNavLinks.style.display = 'flex';
+            mobileNavClose.style.display = 'flex';
+        } else {
+            mobileNavLinks.style.display = 'none';
+            mobileNavClose.style.display = 'none';
+        }
+    });
+}
+
+if (mobileNavClose) {
+    mobileNavClose.addEventListener('click', () => {
+        mobileNavLinks.style.display = 'none';
+        mobileNavClose.style.display = 'none';
+    }
+    );
 }
